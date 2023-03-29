fb2be537090b9a400271360c37cf33aa3bf1986f
Francisco Larrea <francisco.larrea.labs@gmail.com>
2023-03-29 09:55:00 -0300

    01 Historia de usuario Comienzo de Proyecto

---
+++ src/desafiolatam/cl/DemoGit.java
@@ -0,0 +1,10 @@
+package desafiolatam.cl;
+
+public class DemoGit {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hola mundo");
+	}
+
+}
---
+++ src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author Secreadmi
+ *
+ */
+module DemoGit {
+}
