a78f862f86320e425deb3526d131a53f9a968ce3
Francisco Larrea <flarreas@gmail.com>
2023-04-07 22:56:38 -0400

    Cambio a suma 2 numeros

--- src/desafiolatam/cl/DemoGit.java
+++ src/desafiolatam/cl/DemoGit.java
@@ -1,12 +1,50 @@
 package desafiolatam.cl;
 
-public class DemoGit {
+import java.util.Scanner;
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("Hola mundo");
+public class DemoGit {
 		
-		String nombre = "Pedro";
+		public static int numero1,  numero2, resultadosuma, resultadomulti,resultadocompara;
+
+		public static void main(String[] args) {
+			// TODO Auto-generated method stub
+			
+			capturaDatos();
+			resultadoSuma();
+			resultadoMulti();
+		
+		}
+		
+		public static int sumar(int a, int b) {
+			int c;
+			c = a +b;
+			return c;
+		}
+		
+		public static int multiplicar(int a, int b) {
+			int y;
+			y = a*b;
+			return y;
+		}
+		
+		public static void capturaDatos() {
+			Scanner entrada = new Scanner(System.in);
+			System.out.print("Ingrese primer número: ");
+			numero1 = entrada.nextInt();
+			System.out.print("Ingrese segundo número: ");
+			numero2 = entrada.nextInt();	
+			entrada.close();
+		}
+		
+		public static void resultadoSuma() {
+			resultadosuma = sumar(numero1,numero2);
+			System.out.println("Suma: " + resultadosuma);
+		}
+		
+		public static void resultadoMulti() {
+			resultadomulti = multiplicar(numero1, numero2);
+			System.out.println("Multiplicat: " + resultadomulti);
+		}
+
 	}
 
-}
